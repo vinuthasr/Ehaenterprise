63d184c21d68ab5aad5e0a30b8afb277a9e849af
hari krishna reddy <yenugubala.hari@gmail.com>
2019-02-28 16:42:44 +0530

    modified cross origins

--- src/main/java/com/elephant/controller/category/CategoryController.java
+++ src/main/java/com/elephant/controller/category/CategoryController.java
@@ -34,7 +34,7 @@ import com.elephant.utils.CommonUtils;
 
 @RestController
 @RequestMapping("/v1")
-@CrossOrigin(origins="https://sheltered-fortress-53647.herokuapp.com",allowedHeaders="*")
+@CrossOrigin(origins= {"https://eha-admin-app.herokuapp.com","http://localhost:4200","https://eha-user-app.herokuapp.com/"})
 public class CategoryController {
 private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
 	
